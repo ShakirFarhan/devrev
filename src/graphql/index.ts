22e2c7d47caa1e4a0ddf21166ff12bb501a447c0
Shakir Farhan <shakirfarhan123@gmail.com>
2024-02-28 19:17:47 +0530

    changes

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,10 +1,17 @@
 import { ApolloServer } from '@apollo/server';
 import { User } from './user';
 import { Project } from './project';
-
-export default async function createApolloGraphQLServer() {
+import { GraphQLUpload, graphqlUploadExpress } from 'graphql-upload-minimal';
+import { finished } from 'stream/promises';
+import express from 'express';
+export default async function createApolloGraphQLServer(app: any) {
   const graphQLServer = new ApolloServer({
     typeDefs: `#graphql
+      type File {
+    filename: String!
+    mimetype: String!
+    encoding: String!
+  }
     ${User.typeDefs}
     ${Project.typeDefs}
     scalar Upload
@@ -16,9 +23,12 @@ export default async function createApolloGraphQLServer() {
     type Mutation{
      ${User.mutations}
      ${Project.mutations}
-      uploadFile(file:Upload!):String!
+     singleUpload(file: Upload!): File!
+
+     
     }`,
     resolvers: {
+      Upload: GraphQLUpload,
       Query: {
         ...User.resolvers.queries,
         ...Project.resolvers.queries,
@@ -26,10 +36,31 @@ export default async function createApolloGraphQLServer() {
       Mutation: {
         ...User.resolvers.mutations,
         ...Project.resolvers.mutations,
+        singleUpload: async (parent, { file }) => {
+          console.log('here');
+          const { createReadStream, filename, mimetype, encoding } = await file;
+
+          // Invoking the `createReadStream` will return a Readable Stream.
+          // See https://nodejs.org/api/stream.html#stream_readable_streams
+          const stream = createReadStream();
+
+          // This is purely for demonstration purposes and will overwrite the
+          // local-file-output.txt in the current working directory on EACH upload.
+          const out = require('fs').createWriteStream('local-file-output.txt');
+          stream.pipe(out);
+          await finished(out);
+
+          return { filename, mimetype, encoding };
+        },
       },
     },
-    introspection: true,
+    // csrfPrevention: true,
+    // cache: 'bounded',
+
+    // introspection: true,
   });
+
   await graphQLServer.start();
+
   return graphQLServer;
 }
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,25 @@ import { expressMiddleware } from '@apollo/server/express4';
 import createApolloGraphQLServer from './graphql';
 import UserService from './services/user';
 import * as Config from './config/index';
+import { graphqlUploadExpress } from 'graphql-upload-minimal';
+
+import cors from 'cors';
 async function init() {
   const app = express();
   const PORT = Config.PORT || 8000;
 
   app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-
+  app.use(express.urlencoded({ extended: false }));
+  app.use(graphqlUploadExpress());
+  app.use(
+    cors({
+      origin: '*',
+    })
+  );
   app.use(
     '/graphql',
-    expressMiddleware(await createApolloGraphQLServer(), {
+
+    expressMiddleware(await createApolloGraphQLServer(app), {
       context: async ({ req }) => {
         return UserService.deserializeUser(req);
       },
